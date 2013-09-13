668ab209afe4ca71eb3e0a812b1d4955a6690fe0
Dave Paola <dpaola2@gmail.com>
2013-09-12 21:01:18 -0700

    minor reading list stuff

--- static/scripts/services.js
+++ static/scripts/services.js
@@ -3,5 +3,5 @@ function ReadingList() {
 };
 
 ReadingList.prototype.books = ["Little House on the Prairie", "Catch 22"];
-ReadingList.prototype.visible = true;
+ReadingList.prototype.visible = false;
 
